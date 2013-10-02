10a73f96f8f8b4faf1e6a51b98a2b7a38671ac2f
Hari Dosapati <dosapati@yahoo.com>
2013-10-02 12:34:20 -0400

    Initial Version

--- src/com/om/service/CompanyService.java
+++ src/com/om/service/CompanyService.java
@@ -3,6 +3,7 @@ package com.om.service;
 import com.google.gson.Gson;
 import com.om.dao.StockMetadataDAO;
 import com.om.util.DateTimeUtil;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,16 +33,27 @@ public class CompanyService {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/search")
-    public String search(@RequestParam("q") String data ,@Context HttpServletRequest servletRequest) throws Exception {
+    public String search(@RequestParam("q") String data,@Context HttpServletRequest servletRequest) throws Exception {
 
         System.out.println("q = " + data+" , "+servletRequest.getParameter("q"));
-        List<Map<String, Object>> records = stockMetadataDAO.searchCompaniesByAny(servletRequest.getParameter("q"));
+
+        List<Map<String, Object>> records = null;
+        if(StringUtils.equalsIgnoreCase(servletRequest.getParameter("m"),"true")){
+            records = stockMetadataDAO.searchCompaniesBy("ID_BB_GLOBAL",servletRequest.getParameter("q"));
+
+        }else{
+            records = stockMetadataDAO.searchCompaniesByAny(servletRequest.getParameter("q"));
+        }
          Map<String,Object> dataObj = new HashMap<String, Object>();
         dataObj.put("data", records);
 
         Gson gson = new Gson();
         return gson.toJson(dataObj);
     }
+
+
+
+
 
 
 
